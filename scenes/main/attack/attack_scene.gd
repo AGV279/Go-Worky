c07a03268c2d3071fc39c995e304022dd0b17131
gabeh <itsbielrt@gmail.com>
2024-05-18 00:45:12 -0600

    Added signals for mouse clicking on tree areas

--- scenes/main/attack/attack_scene.gd
+++ scenes/main/attack/attack_scene.gd
@@ -1,10 +1,33 @@
-extends Node2D
-
+extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("ready")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_tree_area_1_input_event(viewport, event, shape_idx):
+	#Prevents from calling the signal twice for double click
+	if event is InputEventMouseButton && Input.is_action_just_released("select"):
+		print("To tree 1")
+		
+
+func _on_tree_area_2_input_event(viewport, event, shape_idx):
+	#Prevents from calling the signal twice for double click
+	if event is InputEventMouseButton && Input.is_action_just_released("select"):
+		print("To tree 2")
+		
+
+func _on_tree_area_3_input_event(viewport, event, shape_idx):
+	#Prevents from calling the signal twice for double click
+	if event is InputEventMouseButton && Input.is_action_just_released("select"):
+		print("To tree 3")
+
+
+func _on_tree_area_4_input_event(viewport, event, shape_idx):
+	#Prevents from calling the signal twice for double click
+	if event is InputEventMouseButton && Input.is_action_just_released("select"):
+		print("To tree 4")
+
